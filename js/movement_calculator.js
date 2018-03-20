7532072e096f0b144e595c64ec0de221424a4fe0
megbers <matt.egbers@cengage.com>
2018-03-19 22:25:22 -0400

    Change the title

--- js/movement_calculator.js
+++
@@ -1,9 +0,0 @@
-var boardArea = document.querySelector('canvas');
-
-boardArea.width = window.innerWidth;
-boardArea.height = window.innerHeight;
-
-var context = boardArea.getContext('2d');
-
-
-console.log(boardArea);
